7d75c6397a9c441127397aca394a171c782c4e1e
marcio rodrigues <msrmarcio@hotmail.com>
2019-08-24 13:36:40 -0300

    Adicionado uma classe de exemplo da api via varejo

---
+++ Impacta.Tarefas/Impacta.Tarefas.ClientAPI/Models/ViaVarejoOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impacta.Tarefas.ClientAPI.Models
+{
+	public class ViaVarejoOrder
+	{
+		public int OrderId { get; set; }
+		public int PaymentID { get; set; }
+
+	}
+}
